deef51e758acda27b1e6cc241170d188dcdca8eb
manuabalos <manu.abalos@gmail.com>
2015-11-24 09:38:46 +0100

    Añadida la funcionalidad de eliminar horarios. Fixeados algunos bugs

--- app/controllers/tee_timetables_controller.rb
+++ app/controllers/tee_timetables_controller.rb
@@ -3,11 +3,11 @@ class TeeTimetablesController < ApplicationController
 
   menu_item :config_time_statuses
   before_filter :find_project_by_project_id, :authorize
+  before_filter :set_timetable, only: [:edit, :update, :destroy]
+  before_filter :set_roles, only: [:index, :edit]
+  before_filter :set_weeks, only: [:index, :edit]
   
   def index
-    @roles = Role.uniq.map{|role| [role.name, role.id]}
-    @weeks = ["monday","tuesday","wednesday","thrusday","friday","saturday","sunday"]
-
     @timetable = TeeTimetable.new
     @journals = []
     7.times do |i|
@@ -29,20 +29,15 @@ class TeeTimetablesController < ApplicationController
   end
 
   def edit
-    @roles = Role.uniq.map{|role| [role.name, role.id]}
-    @weeks = ["monday","tuesday","wednesday","thrusday","friday","saturday","sunday"]
-
-    @timetable = TeeTimetable.find(params[:id])
     @journals = @timetable.journals
     @rolestimetable = []
     @timetable.roles.collect{|role| @rolestimetable << role[:id]}
   end
 
-  def update
-    @timetable = TeeTimetable.find(params[:id])  
-    @roles = Role.where(:id => params[:roles])
+  def update 
+    roles = Role.where(:id => params[:roles])
     @timetable.roles.destroy_all
-    @timetable.roles << @roles
+    @timetable.roles << roles
    
     if @timetable.update_attributes(params[:tee_timetable]) 
       redirect_to tee_home_path(:project_id => @project.id)
@@ -52,10 +47,21 @@ class TeeTimetablesController < ApplicationController
   end
 
   def destroy
-    binding.pry
-    timetable = TeeTimetable.find(params[:id])
-    timetable.destroy
-    binding.pry
-    tee_home_path(:project_id => @project.id)
+    @timetable.journals.destroy_all
+    @timetable.destroy
+  
+    redirect_to tee_home_path(:project_id => @project.id)
+  end
+
+  def set_timetable
+    @timetable = TeeTimetable.find(params[:id]) 
+  end
+
+  def set_roles
+    @roles = Role.uniq.map{|role| [role.name, role.id]}
+  end
+
+  def set_weeks
+    @weeks = [{:id => 1, :name =>"monday"},{:id => 2, :name =>"tuesday"},{:id => 3, :name =>"wednesday"},{:id => 4, :name =>"friday"},{:id => 5, :name =>"saturday"},{:id => 0, :name =>"sunday"}]
   end
 end
--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'tee/issue_status_patch'
 require 'tee/project_patch'
-require_dependency 'tee/role_patch'
+require 'tee/role_patch'
 
 Redmine::Plugin.register :tiempo_entre_estados do
   name 'Tiempo Entre Estados plugin'
@@ -12,9 +12,9 @@ Redmine::Plugin.register :tiempo_entre_estados do
 
   #permission :time_statuses, :tee => :index
   project_module :time_statuses do
-    permission :tee_view_config, :tee => [:index, :delete]
+    permission :tee_view_config, :tee => [:index]
     permission :tee_edit_statuses, :tee_prs => [:index, :create]
-    permission :tee_edit_timetables, :tee_timetables => [:index, :create, :edit, :update]
+    permission :tee_edit_timetables, :tee_timetables => [:index, :create, :edit, :update, :destroy]
   end
   menu :project_menu, :config_time_statuses, { :controller => 'tee', :action => 'index' }, :caption => 'Control de tiempos', :last => true, :param => :project_id
 
