71bcad85454f5c3fb72e51fd674d8db62e5da30f
manuabalos <manu.abalos@gmail.com>
2015-11-25 10:18:43 +0100

    Arreglado el problema al modificar un horario cuando se activaba la casilla de laborable

--- assets/javascripts/tee_timetable_journals.js
+++ assets/javascripts/tee_timetable_journals.js
@@ -2,16 +2,19 @@ $(document).ready(function(){
 	$('.workable').on('click', function(){
 		if($(this).attr('checked')){
 			num = $(this).attr('id')
-			$("#tee_timetable_journals_attributes_"+num+"_start_time_4i").attr("disabled",false);
-			$("#tee_timetable_journals_attributes_"+num+"_start_time_5i").attr("disabled",false);
-			$("#tee_timetable_journals_attributes_"+num+"_end_time_4i").attr("disabled",false);
-			$("#tee_timetable_journals_attributes_"+num+"_end_time_5i").attr("disabled",false);
+
+			for(var i=1; i<=5; i++){
+				$("#tee_timetable_journals_attributes_"+num+"_start_time_"+i.toString()+"i").attr("disabled",false);
+				$("#tee_timetable_journals_attributes_"+num+"_end_time_"+i.toString()+"i").attr("disabled",false);
+			}
+			
 		} else {
 			num = $(this).attr('id')
-			$("#tee_timetable_journals_attributes_"+num+"_start_time_4i").attr("disabled",true);
-			$("#tee_timetable_journals_attributes_"+num+"_start_time_5i").attr("disabled",true);
-			$("#tee_timetable_journals_attributes_"+num+"_end_time_4i").attr("disabled",true);
-			$("#tee_timetable_journals_attributes_"+num+"_end_time_5i").attr("disabled",true);
+
+			for(var i=1; i<=5; i++){
+				$("#tee_timetable_journals_attributes_"+num+"_start_time_"+i.toString()+"i").attr("disabled",true);
+				$("#tee_timetable_journals_attributes_"+num+"_end_time_"+i.toString()+"i").attr("disabled",true);
+			}
 
 			$("#tee_timetable_journals_attributes_"+num+"_start_time_4i option:eq(0)").prop('selected',true);
 			$("#tee_timetable_journals_attributes_"+num+"_start_time_5i option:eq(0)").prop('selected',true);
