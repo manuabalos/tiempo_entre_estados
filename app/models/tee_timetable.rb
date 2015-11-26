bdef0de1800f57656f816b9af9ad3f2bcd37a4a4
jresinas <jresinas@emergya.com>
2015-11-26 12:23:39 +0100

    Calculo de tiempo en un intervalo

--- app/models/tee_timetable.rb
+++ app/models/tee_timetable.rb
@@ -2,6 +2,43 @@ class TeeTimetable < ActiveRecord::Base
   unloadable
 
   has_many :journals, :class_name => 'TeeTimetableJournal'
+  scope :journal, ->(date) {includes(:journals).were("journals.week_day = ?", date.wday)}
   has_and_belongs_to_many :roles
 
+  # Tiempo trabajado en una semana completa
+  def week_total_time
+  	journals.inject(0.0){|sum,j| sum + j.day_total_time}
+  end
+
+  # Calcula el tiempo trabajado entre dos fechas para este calendario
+  def get_time(stime, etime)
+  	time = 0
+
+  	if Date.parse(etime.to_s) == Date.parse(stime.to_s)
+  		time += journal(stime).day_time(stime, etime)
+  	else
+  		second_day = Date.parse((stime + 1.day).to_s)
+  		penultimate_day = Date.parse((etime - 1.day).to_s)
+
+  		weeks = ([(penultimate_day - second_day).to_i, 0].max / 7).floor
+
+  		(stime..(etime - weeks.days)).each do |date|
+  			case date
+	  			when stime
+	  				time += journal(date).day_time(date)
+	  			when (etime - weeks.days)
+	  				time += journal(date).day_time(nil, date)
+	  			else
+	  				time += journal(date).day_time
+	  		end
+	  	end
+	  end
+
+	  time
+	end
+
+	# Devuelve el calendario correspondiente al intervalo indicado
+	def self.get_calendar(stime, etime)
+		#where("start_date <= ? AND end_date >= ?", etime, stime)
+	end
 end
--- app/models/tee_timetable_journal.rb
+++ app/models/tee_timetable_journal.rb
@@ -1,7 +1,40 @@
 class TeeTimetableJournal < ActiveRecord::Base
   unloadable
-  scope :week_day, ->(wday) {were("week_day = ?", wday)}
+  #scope :week_day, ->(wday) {were("week_day = ?", wday)}
 
   belongs_to :journals
 
+  # Tiempo trabajado en el día completo
+  def day_total_time
+  	end_time - start_time
+  end
+
+  # Tiempo trabajado desde la hora indicada en stime hasta la hora indicada en etime. Si alguno es nil, se considera el inicio/fin de la jornada
+  def day_time(stime = nil, etime = nil)
+    # Horas de inicio y fin de jornada
+    jstime = normalize_time(start_time)
+    jetime = normalize_time(end_time)
+
+    # Si stime es nulo, toma el valor del inicio de la jornada
+    if stime.present?
+      stime = normalize_time(stime)
+    else
+      stime = jstime
+    end
+
+    # Si etime es nulo, toma el valor del fin de la jornada
+    if etime.present?
+      etime = normalize_time(etime) 
+    else
+      etime = jetime
+    end
+
+    [([etime, jetime].min.to_i - [stime, jstime].max.to_i), 0].max
+  end
+
+  private
+  # Dado que se está trabajando con horas y no días, establecemos los datetimes al día de hoy
+  def normalize_time(time)
+  	time.change(:day => Date.today.day, :month => Date.today.month, :year => Date.today.year)
+  end
 end
