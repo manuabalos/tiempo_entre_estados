192500000cc797b5d47735e6d2bc2d37d573ee0f
jresinas <jresinas@emergya.com>
2015-12-09 11:43:15 +0100

    Modificación de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,21 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 RedmineApp::Application.routes.draw do
-	match 'tee/:action', :to => 'tee', as: 'tee_home'
-	resources :tee_prs
-	resources :tee_timetables do
-		resources :tee_timetables_journals
-	end
-	resources :tee_holidays
-
-	get 'issues/:id/stats_total_time', to: 'issues#stats_total_time', as: 'stats_total_time'
+	scope '/projects/:project_id/' do
+		match 'tiempo_entre_estados', to: 'tee#index', as: 'tee_home'
 	
+		#match ':role_id/statuses', to: 'tee_prs#index'
+		#match ':role_id/statuses/:action', to: 'tee_prs'
+		scope ':role_id/' do
+			resources :statuses, :controller => 'tee_prs', :as => 'tee_prs'
+		end
+
+		resources :timetables, :controller => 'tee_timetables', :as => 'tee_timetables' do
+			resources :tee_timetables_journals
+		end
+		resources :holidays, :controller => 'tee_holidays', :as => 'tee_holidays'
+	end
+
+
+	get '/issues/:id/stats_total_time', to: 'issues#stats_total_time', as: 'stats_total_time'
 end
